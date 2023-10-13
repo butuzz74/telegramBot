d5b3365fa943c43ec5d7fc43e4f3c2b17c86c5df
Alexey <butuzz74@gmail.com>
2023-10-13 15:33:54 +0300

    fit

--- src/components/pages/FormSend.jsx
+++ src/components/pages/FormSend.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import TextField from "../form/TextField";
 import Button from "../common/Button";
 import { TelegramContext } from "../../context/context";
